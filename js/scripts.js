e42f928d2a7bd064f31b2cc3ed42ce0e02eed29e
dimalepel <dimalepel@mail.ru>
2020-05-07 02:25:17 +0300

    update viewport

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 (function () {
+  /* Проверка наличия класса у элемента */
   function hasClass(el, cls) {
     return el.className && new RegExp("(\\s|^)" + cls + "(\\s|$)").test(el.className);
   }
 
+  /* Открытие меню */
   let menuOpen = document.getElementById('open-menu');
   if (menuOpen) {
     let body = document.querySelector('body');
@@ -19,5 +21,15 @@
         body.classList.add('overflow');
       }
     });
+  }
+  
+  /* Подмена вьюпорта */
+  const viewPort = document.querySelector('[name="viewport"]');
+  if (viewPort) {
+    if (screen.width < 980) {
+      viewPort.setAttribute('content','width=320, user-scalable=yes');
+    } else {
+      viewPort.setAttribute('content','width=980, user-scalable=yes');
+    }
   }
 })();
